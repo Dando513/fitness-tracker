54ef4f71374656d01bb56514c11f3f9d424c9f35
Dando513 <49767042+Dando513@users.noreply.github.com>
2020-09-17 21:31:06 -0400

    fixed api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,21 @@
 const router = require("express").Router();
 let db = require("../models");
 
-router.post("/api/workouts", ({ body }, res) => {
-  db.Workout.create(body)
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+  .sort({ date: -1 })
+   .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  var id = req.params.id
+  db.Workout.findByIdAndUpdate(id, { $push: {exercises: req.body}})
+    .sort({ date: -1 })
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
@@ -11,7 +24,17 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
-router.get("/api/workouts", (req, res) => {
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create(req.body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
   db.Workout.find({})
     .sort({ date: -1 })
     .then(dbWorkout => {
